917c1cd6a32ace1e5833e418861cff8aa6ba28f3
MartinusChoi <martinus.choi@gmail.com>
2025-09-04 00:38:16 +0900

    base server 구축 연습

--- src/mcp_servers/base_server/base_server.py
+++ src/mcp_servers/base_server/base_server.py
@@ -2,13 +2,19 @@
 Base MCP Server Class.
 """
 
+import time
 import logging
 
 from pydantic import BaseModel, ConfigDict, Field
-from typing import Any
+from typing import Any, Literal
 
 from abc import ABC, abstractmethod
 
+from fastmcp.server.http import StarletteWithLifespan
+from fastmcp.server.middleware import Middleware, MiddlewareContext
+
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 # ------------------------------------------------------------------------------------------------
 # Standard Succuess/Error Response Models.
@@ -61,10 +67,13 @@ class BaseMCPServer(ABC):
     Implment common features of all mcp servers.
     """
 
+    MCP_PATH = "/mcp"
+
     def __init__(
         self,
-        server_name,
-        server_instruction,
+        server_name:str|None=None,
+        server_instruction:str|None=None,
+        transport:Literal["streamable-http", "stdio"]="streamable-http"
     ) -> None:
         
         from fastmcp import FastMCP
@@ -83,6 +92,9 @@ class BaseMCPServer(ABC):
         self.logger.addHandler(stream_handler)
         self.mcp.logger = self.logger
 
+        # set transport method
+        self.transport = transport
+
         # Initialize MCP Tool clients
         self._initialize_clients()
 
@@ -90,17 +102,20 @@ class BaseMCPServer(ABC):
         self._registry_tools()
 
         # Add Middlewares
+        self.install_middlewares()
     
 
     # ------------------------------------------------------------------------------------------------
     # Settings for MCP Tools. Should implement specifics for specific MCP Server.
     # ------------------------------------------------------------------------------------------------
+    @abstractmethod
     def _initialize_clients(self)->None:
         """
         Initialize MCP Tool clients.
         """
         raise NotImplementedError("Initialize Method for MCP Tools should be implemented.")
-
+    
+    @abstractmethod
     def _registry_tools(self)->None:
         """
         Define and Registry MCP Tools in Server.
@@ -115,11 +130,22 @@ class BaseMCPServer(ABC):
         query:str,
         success:bool = True,
         data: str | Any | None = None,
+        **kwargs
     ) -> dict[str, Any]:
+        """
+        Create Success Response with Standard Response Models.
+
+        Args:
+            success: Boolean value of Success/Failure.
+            data: Data contents for Request.
+            query: Requested initial query.
+            **kwargs: extra fields.
+        """
         _response = ResponseModel(
             success=success,
             query=query,
-            data=data
+            data=data,
+            **kwargs
         )
 
         return _response.model_dump(exclude_none=True)
@@ -128,16 +154,143 @@ class BaseMCPServer(ABC):
         self,
         error: str,
         query: str,
-        func_name:Any|None = None
+        func_name:Any|None = None,
+        **kwargs
     ) -> dict[str, Any]:
+        """
+        Create Error Response with Standard Response Models.
+
+        Args:
+            success: Boolean value of Success/Failure (always False).
+            error: Error Message occured during requested process.
+            query: Requested initial query.
+            func_name: Function name that error occured.
+            **kwargs: extra fields.
+        """
         _error_response = ErrorResponseModel(
             success=False,
             error=error,
             query=query,
-            func_name=func_name
+            func_name=func_name,
+            **kwargs
         )
 
         return _error_response.model_dump(exclude_none=True)
     
-    
+    # ------------------------------------------------------------------------------------------------
+    # Create Starllete ASGI server app.
+    # ------------------------------------------------------------------------------------------------
+    async def create_app(self) -> StarletteWithLifespan:
+        """
+        Create Http app (Starlette ASGI Server app)
+        """
+
+        if not getattr(self, "_health_endpoint_added", None):
+            self.mcp.custom_route("/health", methods=["GET"], include_in_schema=True)
+            async def health_check(request:Request)->JSONResponse:
+                _response = ResponseModel(
+                    success=True,
+                    data="ok",
+                    query="MCP Server Health Check."
+                )
+
+                return JSONResponse(_response)
+            
+            setattr(self, "_health_endpoint_added", True)
         
+        return self.mcp.http_app(
+            path=self.MCP_PATH,
+            transport=self.transport
+        )
+    
+    # ------------------------------------------------------------------------------------------------
+    # Add Middleware in MCP Server.
+    # ------------------------------------------------------------------------------------------------
+    def install_middlewares(self)->None:
+        """
+        Add MCP Server Middelwares in server.
+        
+        middleware list:
+            ErrorHandlerMiddleware
+            LoggingMiddleware
+            TimingMiddelware
+        """
+        
+        self.mcp.add_middleware(self.ErrorHandlingMiddleware)
+        self.mcp.add_middleware(self.TimingMiddelware)
+        self.mcp.add_middleware(self.LoggingMiddleware)
+    
+    # ------------------------------------------------------------------------------------------------
+    # Core Middleware Classes.
+    # ------------------------------------------------------------------------------------------------
+    class ErrorHandlingMiddleware(Middleware):
+        """
+        Error Event Handling Middleware.
+        """
+
+        async def on_call_tool(self, context:MiddlewareContext, call_next):
+            
+            try:
+                return await call_next(context=context)
+            except Exception as error:
+                try:
+                    server = context.fastmcp_context.fastmcp
+                    logger = getattr(server, "logger", None)
+
+                    if logger:
+                        logger.error(f"Tool Error : {error}", exc_info=True)
+                    else:
+                        raise UserWarning('Logger is not Setted in this MCP Server.')
+                except Exception as error:
+                    raise error
+    
+    class LoggingMiddleware(Middleware):
+        """
+        Middleware for Log Process in MCP Server.
+        """
+
+        async def on_call_tool(self, context:MiddlewareContext, call_next):
+            try:
+                server = context.fastmcp_context.fastmcp
+                logger = getattr(server, "logger", None)
+
+                if logger:
+                    meta = {
+                        'request_id' : getattr(context.fastmcp_context, "request_id", None),
+                        'client_id' : getattr(context.fastmcp_context, 'client_id', None),
+                        'session_id' : getattr(context.fastmcp_context, 'session_id', None)
+                    }
+                    logger.info(f"Start Process for Request : {meta}")
+                else:
+                    UserWarning("Logger is not Setted in this MCP Server.")
+                
+                response = await call_next(context=context)
+
+                if logger:
+                    duration_ms = context.fastmcp_context.get_state('duration_ms')
+                    logger.info(f"Requst Succeed for duration time (ms) : {duration_ms}")
+                else:
+                    UserWarning("Logger is not Setted in this MCP Server.")
+                
+                return response
+            except Exception as error:
+                raise error
+    
+    class TimingMiddelware(Middleware):
+        """
+        Middleware for Calculation Processing time
+        """
+
+        async def on_call_tool(self, context:MiddlewareContext, call_next):
+
+            start_time = time.perf_counter()
+
+            try : 
+                return await call_next(context=context)
+            finally:
+                duration_ms = (time.perf_counter() - start_time) * 1000.0
+
+                try:
+                    context.fastmcp_context.set_state('duration_ms', duration_ms)
+                except Exception as error:
+                    raise error
